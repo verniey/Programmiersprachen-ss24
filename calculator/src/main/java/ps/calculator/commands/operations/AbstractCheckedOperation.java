461429543267ac88173defce07b7c0a68e333cfe
Veronika <veronika@Veronikas-MacBook-Pro.local>
2024-08-05 09:32:12 +0200

    Refactored AbstractCheckedOperation to use a flexible stack size check with getRequiredStackSize

--- calculator/src/main/java/ps/calculator/commands/operations/AbstractCheckedOperation.java
+++ calculator/src/main/java/ps/calculator/commands/operations/AbstractCheckedOperation.java
@@ -2,13 +2,27 @@ package ps.calculator.commands.operations;
 
 import ps.calculator.CalculatorContext;
 
+/**
+ * An abstract operation that checks if the stack has enough elements before execution.
+ */
 public abstract class AbstractCheckedOperation implements Operation {
+
     @Override
     public void execute(CalculatorContext context) {
-        if (context.getDataStack().size() < 2) {
-            throw new IllegalStateException("The stack must contain at least two elements.");
+        // Check if the stack has enough elements
+        if (context.getDataStack().size() < getRequiredStackSize()) {
+            throw new IllegalStateException("The stack must contain at least " + getRequiredStackSize() + " elements.");
         }
         performOperation(context);
     }
+
+    /**
+     * Specifies the number of stack elements required by the operation.
+     */
+    protected abstract int getRequiredStackSize();
+
+    /**
+     * Perform the actual operation.
+     */
     protected abstract void performOperation(CalculatorContext context);
 }
